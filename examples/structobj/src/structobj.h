a9c4f89f1bcd5c3f12d9e2eef40760be9b86188f
Gustavo6046 <rehermann6046@gmail.com>
2020-05-24 15:41:21 -0300

    Removed internal double-underscore prefix (aka reserved keywords)

--- examples/structobj/src/structobj.h
+++ examples/structobj/src/structobj.h
@@ -20,19 +20,19 @@
 // This defines an interface named I.
 // Concrete 'self' pointers (ie to implementations of
 // interfaces)
-#define INTERFACE_BEGIN(I)                              typedef struct __intf_##I##_s {
-#define INTERFACE_MEMBER(I, RTYPE, NAME, ...)               RTYPE (*__intf_##I##_func_##NAME)(struct __intf_##I##_s *__intfself, void *__implself, __VA_ARGS__);
-#define INTERFACE_END(I)                                } __intf_##I; extern __intf_##I __intf_##I##_default;
+#define INTERFACE_BEGIN(I)                              typedef struct _intf_##I##_s {
+#define INTERFACE_MEMBER(I, RTYPE, NAME, ...)               RTYPE (*_intf_##I##_func_##NAME)(struct _intf_##I##_s *_intfself, void *_implself, __VA_ARGS__);
+#define INTERFACE_END(I)                                } _intf_##I; extern _intf_##I _intf_##I##_default;
 
-#define INTERFACE_POST_MEMBER(I, RTYPE, NAME, ...)      typedef RTYPE (*__intf_##I##_func_##NAME##_t)(__intf_##I *__intfself, void *__implself, __VA_ARGS__);\
-                                                        static inline __intf_##I##_func_##NAME##_t __intf_##I##_get_func_##NAME(__intf_##I *__intfself, const char *__implname) {\
-                                                            if (__intfself->__intf_##I##_func_##NAME)\
-                                                                return __intfself->__intf_##I##_func_##NAME;\
-                                                            else if (__intf_##I##_default.__intf_##I##_func_##NAME)\
-                                                                return __intf_##I##_default.__intf_##I##_func_##NAME;\
+#define INTERFACE_POST_MEMBER(I, RTYPE, NAME, ...)      typedef RTYPE (*_intf_##I##_func_##NAME##_t)(_intf_##I *_intfself, void *_implself, __VA_ARGS__);\
+                                                        static inline _intf_##I##_func_##NAME##_t _intf_##I##_get_func_##NAME(_intf_##I *_intfself, const char *_implname) {\
+                                                            if (_intfself->_intf_##I##_func_##NAME)\
+                                                                return _intfself->_intf_##I##_func_##NAME;\
+                                                            else if (_intf_##I##_default._intf_##I##_func_##NAME)\
+                                                                return _intf_##I##_default._intf_##I##_func_##NAME;\
                                                             else {\
-                                                                __intf_nul(__implname, #NAME, #I);\
-                                                                return __intf_##I##_default.__intf_##I##_func_##NAME;\
+                                                                _intf_nul(_implname, #NAME, #I);\
+                                                                return _intf_##I##_default._intf_##I##_func_##NAME;\
                                                             }\
                                                         }
 
@@ -41,13 +41,13 @@
 // Keep in mind you can't use the 'self' symbol in
 // the interface, as there is no concrete 'self'
 // known here. Use INTFCALL instead.
-#define IDEFAULT_FUNC_DECLARE(I, RTYPE, NAME, ...)      RTYPE __intf_##I##_default_##NAME(struct __intf_##I##_s *__intfself, void *__implself, __VA_ARGS__);
-#define IDEFAULT_FUNC_BEGIN(I, RTYPE, NAME, ...)        RTYPE __intf_##I##_default_##NAME(struct __intf_##I##_s *__intfself, void *__implself, __VA_ARGS__) {
+#define IDEFAULT_FUNC_DECLARE(I, RTYPE, NAME, ...)      RTYPE _intf_##I##_default_##NAME(struct _intf_##I##_s *_intfself, void *_implself, __VA_ARGS__);
+#define IDEFAULT_FUNC_BEGIN(I, RTYPE, NAME, ...)        RTYPE _intf_##I##_default_##NAME(struct _intf_##I##_s *_intfself, void *_implself, __VA_ARGS__) {
 #define IDEFAULT_FUNC_END()                             }
 
-#define IDEFAULT_BEGIN(I)                               __intf_##I __intf_##I##_default = {
-#define IDEFAULT_MEMBER(I, NAME)                            .__intf_##I##_func_##NAME = &__intf_##I##_default_##NAME,
-#define IDEFAULT_NONE(I, NAME)                              .__intf_##I##_func_##NAME = NULL,
+#define IDEFAULT_BEGIN(I)                               _intf_##I _intf_##I##_default = {
+#define IDEFAULT_MEMBER(I, NAME)                            ._intf_##I##_func_##NAME = &_intf_##I##_default_##NAME,
+#define IDEFAULT_NONE(I, NAME)                              ._intf_##I##_func_##NAME = NULL,
 #define IDEFAULT_END()                                  };
 
 // This precedes the arguments, when calling an
@@ -55,37 +55,37 @@
 // the interface itself. Can only be called from
 // within an implementation of one of said 
 // interface's members.
-#define INTFCALL(I, FNAME, ...)                         (*__intfself->__intf_##I##_func_##FNAME)(__intfself, __implself, __VA_ARGS__)
+#define INTFCALL(I, FNAME, ...)                         (*_intfself->_intf_##I##_func_##FNAME)(_intfself, _implself, __VA_ARGS__)
 
 // Each implementation of an interface's method must
 // me defined with IMPL preprocessors.
 
-#define IMPL_DECLARE(I, NAME)                           extern __intf_##I __intf_##I##_impl_##NAME;
-#define IMPLOF(I, NAME)                                 &__intf_##I##_impl_##NAME
+#define IMPL_DECLARE(I, NAME)                           extern _intf_##I _intf_##I##_impl_##NAME;
+#define IMPLOF(I, NAME)                                 &_intf_##I##_impl_##NAME
 
-#define IMPL_BEGIN(I, NAME)                             __intf_##I __intf_##I##_impl_##NAME = {
-#define IMPL_MEMBER(I, T, NAME)                             .__intf_##I##_func_##NAME = &__impl_##I##_##T##_func_##NAME,
+#define IMPL_BEGIN(I, NAME)                             _intf_##I _intf_##I##_impl_##NAME = {
+#define IMPL_MEMBER(I, T, NAME)                             ._intf_##I##_func_##NAME = &_impl_##I##_##T##_func_##NAME,
 #define IMPL_END()                                      };
 
 // Implementations' functions must be declare outside
 // the implementation itself.
-#define IMPL_FUNC_BEGIN(I, T, RTYPE, NAME, ...)         RTYPE __impl_##I##_##T##_func_##NAME(struct __intf_##I##_s *__intfself, void *__implself, __VA_ARGS__) {\
-                                                            T *self = (T*)__implself;
+#define IMPL_FUNC_BEGIN(I, T, RTYPE, NAME, ...)         RTYPE _impl_##I##_##T##_func_##NAME(struct _intf_##I##_s *_intfself, void *_implself, __VA_ARGS__) {\
+                                                            T *self = (T*)_implself;
 #define IMPL_FUNC_END()                                 }
 
 // INTF calls can be used in implementation methods.
 // However, only IMPL calls can be used to call an
 // implementation method, whether it is from inside
 // or outside said implementations.
-void* (*__impl_curr_fptr)();
+void* (*_impl_curr_fptr)();
 
-#define IMPLFPTR(I, T, IMPL, FNAME)                     (__intf_##I##_get_func_##FNAME##(IMPL, #T))
-#define IMPLFPTRSET(I, T, IMPL, FNAME)                  (__impl_curr_fptr = __intf_##I##_get_func_##FNAME(IMPL, #T))
+#define IMPLFPTR(I, T, IMPL, FNAME)                     (_intf_##I##_get_func_##FNAME##(IMPL, #T))
+#define IMPLFPTRSET(I, T, IMPL, FNAME)                  (_impl_curr_fptr = _intf_##I##_get_func_##FNAME(IMPL, #T))
 
-#define IMPLCALL(I, T, FNAME, ...)                      (IMPLFPTRSET(I, T, __intfself, FNAME), (*__impl_curr_fptr)(__intfself, self, __VA_ARGS__))
-#define IMPLCALLON(I, T, IMPL, FNAME, ...)              (IMPLFPTRSET(I, T, &__intf_##I##_impl_##T, FNAME), (*__impl_curr_fptr)(&__intf_##I##_impl_##T, IMPL, __VA_ARGS__))
+#define IMPLCALL(I, T, FNAME, ...)                      (IMPLFPTRSET(I, T, _intfself, FNAME), (*_impl_curr_fptr)(_intfself, self, __VA_ARGS__))
+#define IMPLCALLON(I, T, IMPL, FNAME, ...)              (IMPLFPTRSET(I, T, &_intf_##I##_impl_##T, FNAME), (*_impl_curr_fptr)(&_intf_##I##_impl_##T, IMPL, __VA_ARGS__))
 
-static inline void __intf_nul(const char *t, const char *fname, const char *i);
+static inline void _intf_nul(const char *t, const char *fname, const char *i);
 
 //---------------------- exiting  <structobj.h>
 
@@ -93,7 +93,7 @@ static inline void __intf_nul(const char *t, const char *fname, const char *i);
 
 //---------------------- entering <structobj.h> impl mode
 // Internal implementations.
-static inline void __intf_nul(const char *t, const char *fname, const char *i) {
+static inline void _intf_nul(const char *t, const char *fname, const char *i) {
     fprintf(stderr,"Struct-Obj %s does not implement %s of interface %s, nor is there a default! Aborted.", t, fname, i);
     abort();
 }
